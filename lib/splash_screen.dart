0b3979ed707a927fef1ba04de0058dda8ae85bf5
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-08-27 12:33:26 +0530

    Add page 2

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,8 +10,53 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(),
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          //crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 4 * screenSize.height / 9,
+              child: Image.asset("assets/images/Group 9image_grp.png"),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: [
+                Text("Let's get Closer"),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              "The best place to meet your future partner.",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              height: 40,
+              width: 5 * screenSize.width / 6,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.red[400],
+              ),
+              child: Center(
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
