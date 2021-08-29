26ebc20b6141fdb9a848935632df1008d868e5ae
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-08-30 01:28:52 +0530

    Add all

--- lib/front_screen.dart
+++ lib/front_screen.dart
@@ -1,5 +1,6 @@
 import 'package:assignment/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class FrontScreen extends StatefulWidget {
   const FrontScreen({Key? key}) : super(key: key);
@@ -25,14 +26,62 @@ class _FrontScreenState extends State<FrontScreen> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
+      body: Container(
+        width: double.infinity,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Transform(
+              transform: Matrix4.diagonal3Values(1, 1, 1),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 550,
+                    height: 550,
+                    decoration: BoxDecoration(
+                      color: Colors.pink.withOpacity(0.03),
+                      borderRadius: BorderRadius.circular(400.0),
+                    ),
+                  ),
+                  Container(
+                    width: 400,
+                    height: 400,
+                    decoration: BoxDecoration(
+                      color: Colors.pink.withOpacity(0.05),
+                      borderRadius: BorderRadius.circular(400.0),
+                    ),
+                  ),
+                  Container(
+                    width: 250,
+                    height: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.pink.withOpacity(0.07),
+                      borderRadius: BorderRadius.circular(400.0),
+                    ),
+                  ),
+                  Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.pink.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(400.0),
+                    ),
+                  ),
+                  Container(
+                    child: Image.asset(
+                      "assets/images/XMLID 1.png",
+                      width: 120,
+                    ),
+                  ),
+                ],
+              ),
+            ),
             Container(
               child: Text(
                 "Find Your Soulmate",
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 25,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'bold',
                 ),
