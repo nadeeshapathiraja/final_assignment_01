12067b14ec29709a20c604e496431593bc901f3c
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-08-29 23:09:35 +0530

    Start front page

--- lib/front_screen.dart
+++ lib/front_screen.dart
@@ -26,69 +26,21 @@ class _FrontScreenState extends State<FrontScreen> {
     var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Center(
-        child: Center(
-          child: Stack(
-            children: <Widget>[
-              CirclePath(
-                path: "assets/images/Ellipse 5Login.png",
-                height_size: 700,
-              ),
-              CirclePath(
-                path: "assets/images/Ellipse 6Login.png",
-                height_size: 600,
-              ),
-              CirclePath(
-                path: "assets/images/Ellipse 7Login.png",
-                height_size: 400,
-              ),
-              CirclePath(
-                path: "assets/images/Group 10.png",
-                height_size: 150,
-              ),
-              CirclePath(
-                path: "assets/images/XMLID 1.png",
-                height_size: 200,
-              ),
-              Container(
-                alignment: Alignment.bottomCenter,
-                margin: EdgeInsets.only(bottom: 200),
-                child: Text(
-                  "Find Your Soulmate",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'bold',
-                  ),
+        child: Column(
+          children: [
+            Container(
+              child: Text(
+                "Find Your Soulmate",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'bold',
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-class CirclePath extends StatelessWidget {
-  const CirclePath({
-    Key? key,
-    required this.path,
-    required this.height_size,
-  }) : super(key: key);
-  final String path;
-  final double height_size;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Image.asset(
-        path,
-        height: height_size,
-        //width: double.infinity,
-      ),
-    );
-  }
-}
-//Image.asset("assets/images/Ellipse 7.png", ),
-
